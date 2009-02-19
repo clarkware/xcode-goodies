d9873bf23ac64f898ca2a6e30d17eb6adb5a9c0c
Mike Clark <mike@clarkware.com>
2009-02-19 16:20:12 -0700

    Override the init macro for my preferred style.

--- scripts/user-defaults.sh
+++ scripts/user-defaults.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
+# Xcode User Default Overrides
+
+# Specifies whether Xcode displays a warning when an undo operation on an 
+# open file would take it to a state prior to the file's last save operation.
+#
+# Default: YES
 defaults write com.apple.XCode XCShowUndoPastSaveWarning NO
-defaults write com.apple.XCode XCCodeSenseFormattingOptions -dict FunctionBlockSeparator " "
+
+# FunctionBlockSeparator: Whitespace after a method or function name and 
+# argument-list declarations and its body.  Default: "\n"
+#
+# PreMethodDeclSpacing: Whitespace between the parenthesized return type 
+# and the method name in an Objective-C method declaration. Default: " " 
+defaults write com.apple.XCode XCCodeSenseFormattingOptions \
+  -dict FunctionBlockSeparator " " PreMethodDeclSpacing ""
