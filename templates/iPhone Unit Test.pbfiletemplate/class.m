aca141669c885b3b060ea51739b8c54baaae662b
Mike Clark <mike@clarkware.com>
2009-02-18 19:30:00 -0700

    File template for iPhone unit testing.

---
+++ templates/iPhone Unit Test.pbfiletemplate/class.m
@@ -0,0 +1,17 @@
+#import "GTMSenTestCase.h"
+
+@interface «FILEBASENAMEASIDENTIFIER» : SenTestCase 
+
+@end
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+-(void) setUp {
+
+}
+
+-(void) testSomethingAlready {
+    STAssertTrue(YES, nil);
+}
+
+@end
