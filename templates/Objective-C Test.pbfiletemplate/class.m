34352f7e50861568d94ce4f93da7b44668ea976d
Mike Clark <mike@clarkware.com>
2009-03-26 13:59:46 -0600

    Template for an Objective-C test case, all in one file.

---
+++ templates/Objective-C Test.pbfiletemplate/class.m
@@ -0,0 +1,13 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface «FILEBASENAMEASIDENTIFIER» : SenTestCase {
+}
+@end
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+- (void)testSomethingAlready {
+    STAssertTrue(NO, nil);
+}
+
+@end
